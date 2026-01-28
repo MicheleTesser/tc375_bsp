4d5a133679c2ced8766cad866b6282e7d3d1805f
Michele <michele.tesser@raceup.it>
2026-01-28 03:07:00 +0100

    fix time

--- driver/timer.c
+++ driver/timer.c
@@ -1,16 +1,24 @@
 #include "timer.h"
-#include "FreeRTOSConfig.h"
 #include "bsp.h"
+
+static inline uint32_t stm_src_mhz(void) { return (uint32_t)UC_STM_CLOCK; }
+static inline uint32_t stm_div(void) { return SCU_CCUCON0.B.STMDIV; }
 
 void timer_init(void) {}
 
 time_var_microseconds timer_time_now(void) {
-  uint32_t raw = bsp_uc_stm_GetChannelCurrentValue(0U);
-  return (time_var_microseconds)raw;
+  uint32_t coreId = (uint32_t)bsp_uc_core_GetCurrentCore();
+  uint32_t ticks = bsp_uc_stm_GetChannelCurrentValue(coreId);
+  return (time_var_microseconds)(((uint64_t)ticks * (uint64_t)stm_div()) /
+                                 (uint64_t)stm_src_mhz());
 }
 
-void timer_wait(const time_var_microseconds ticks) {
-  bsp_uc_stm_Wait(configSTM_CLOCK_HZ, (uint32_t)ticks);
+void timer_wait(const time_var_microseconds time_us) {
+  int coreId = bsp_uc_core_GetCurrentCore();
+  uint32_t stm_val = bsp_uc_stm_GetChannelCurrentValue(coreId);
+  while ((uint32_t)(bsp_uc_stm_GetChannelCurrentValue(coreId) - stm_val) <
+         (time_us * stm_src_mhz() / stm_div())) {
+  }
 }
 
 time_var_microseconds get_tick_from_millis(uint32_t millis) {
@@ -18,14 +26,15 @@ time_var_microseconds get_tick_from_millis(uint32_t millis) {
 }
 
 time_var_microseconds get_tick_from_micros(uint32_t micros) {
-  return (time_var_microseconds)((micros * (uint64_t)configSTM_CLOCK_HZ) /
-                                 1000000ULL);
+  return (time_var_microseconds)(((uint64_t)micros * (uint64_t)stm_src_mhz()) /
+                                 (uint64_t)stm_div());
 }
 
 float get_millis_from_tick(time_var_microseconds ticks) {
-  return (float)((ticks * 1000.0f) / (float)configSTM_CLOCK_HZ);
+  return ((float)ticks * (float)stm_div()) / ((float)stm_src_mhz() * 1000.0f);
 }
 
 float get_seconds_from_tick(time_var_microseconds ticks) {
-  return (float)(ticks / (float)configSTM_CLOCK_HZ);
+  return ((float)ticks * (float)stm_div()) /
+         ((float)stm_src_mhz() * 1000000.0f);
 }
